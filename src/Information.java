a7eb12b44682e1ef5db40c1b72105a9990ea36e8
DenTynkovan <denistynkovan5@gmail.com>
2022-10-20 14:04:35 +0300

    first commit

---
+++ src/Information.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Information {
+
+    public static void userInformation() {
+        ArrayList<String> alias = new ArrayList<>();
+        HashSet<String> email = new HashSet<>();
+        HashMap<String, String> userKey = new HashMap<>();
+
+        for (int i = 0; i < 10; i++) {
+            alias.add("Bob");
+        }
+    }
+}
